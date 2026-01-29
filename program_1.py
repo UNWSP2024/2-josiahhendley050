e0962e9331afe0d6d9472f431cbedb09ce02794f
josiahhendley050 <josiahhendley050@gmail.com>
2026-01-28 19:58:22 -0600

    Update program_1.py

--- program_1.py
+++ program_1.py
@@ -1,5 +1,9 @@
 def personal_information():
-    print('...')
+    print("Name: Bobby Stormwood")
+    print("Address: 1420 Oak Ridge Lane")
+    print("City/State/ZIP: Silver Falls, MN 55674")
+    print("Phone: (555) 298-3337")
+    print("College Major: Computer Science")
 
 
 # Line which calls the above function.
